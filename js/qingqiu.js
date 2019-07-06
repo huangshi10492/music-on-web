415e8ccb70a4df7bb7bddcf7b1a07f8c0722d85a
huangshi <13380417527@163.com>
2019-07-06 16:44:00 +0800

    7.6

--- js/qingqiu.js
+++ js/qingqiu.js
@@ -1,24 +1,24 @@
 
 function AcopyText() {
-    var wyid = document.getElementById('wyso').value;
-layui.use('table', function(){
+  var wyid = document.getElementById('wyso').value;
+  layui.use('table', function () {
     var table = layui.table;
-    
+
     table.render({
       elem: '#test'
-      ,url:"https://api.mlwei.com/music/api/wy/?key=523077333&type=so&id=" + wyid
-      ,title: 'item'
-      ,cols: [[
-        {field:'id', title:'ID', width:250, type: "normal", }
-        ,{field:'title', title:'用户名', width:300, type: "normal"}
-        ,{field:'author', title:'邮箱', width:200, type: "normal"}
-        ,{field:'url', title:'性别', width:840, type: "normal"}
+      , url: "https://api.mlwei.com/music/api/wy/?key=523077333&type=so&id=" + wyid
+      , title: 'item'
+      , cols: [[
+        { field: 'id', title: 'ID', type: "normal", }
+        , { field: 'title', title: '曲名', type: "normal" }
+        , { field: 'author', title: '歌手', type: "normal" }
+        , { field: 'url', title: '地址', type: "normal" }
       ]]
-      ,response: {
+      , response: {
         statusName: "Code",
         statusCode: "OK"//重新规定成功的状态码为 200，table 组件默认为 0
       }
-      ,parseData: function(res){ //将原始数据解析成 table 组件所规定的数据
+      , parseData: function (res) { //将原始数据解析成 table 组件所规定的数据
         return {
           "Code": res.Code, //解析接口状态
           "data": res.Body //解析数据列表
@@ -28,25 +28,25 @@ layui.use('table', function(){
   });
 };
 function BcopyText() {
-    var qqid = document.getElementById('qqso').value;
-layui.use('table', function(){
+  var qqid = document.getElementById('qqso').value;
+  layui.use('table', function () {
     var table = layui.table;
-    
+
     table.render({
       elem: '#test'
-      ,url:"https://api.mlwei.com/music/api/?key=523077333&type=so&id=" + qqid
-      ,title: 'item'
-      ,cols: [[
-        {field:'mid', title:'ID', width:250, type: "normal", }
-        ,{field:'title', title:'用户名', width:300, type: "normal"}
-        ,{field:'author', title:'邮箱', width:200, type: "normal"}
-        ,{field:'url', title:'性别', width:840, type: "normal"}
+      , url: "https://api.mlwei.com/music/api/?key=523077333&type=so&id=" + qqid
+      , title: 'item'
+      , cols: [[
+        { field: 'mid', title: 'ID', type: "normal", }
+        , { field: 'title', title: '曲名', type: "normal" }
+        , { field: 'author', title: '歌手', type: "normal" }
+        , { field: 'url', title: '地址', type: "normal" }
       ]]
-      ,response: {
+      , response: {
         statusName: "Code",
         statusCode: "OK"//重新规定成功的状态码为 200，table 组件默认为 0
       }
-      ,parseData: function(res){ //将原始数据解析成 table 组件所规定的数据
+      , parseData: function (res) { //将原始数据解析成 table 组件所规定的数据
         return {
           "Code": res.Code, //解析接口状态
           "data": res.Body //解析数据列表
