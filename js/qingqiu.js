7b20614e7d95a26a9280b4ab704f0e73a2ccd07c
huangshi <13380417527@163.com>
2019-07-11 21:20:25 +0800

    7.11

--- js/qingqiu.js
+++ js/qingqiu.js
@@ -1,4 +1,5 @@
 
+
 function AcopyText() {
   var wyid = document.getElementById('wyso').value;
   layui.use('table', function () {
@@ -8,13 +9,13 @@ function AcopyText() {
       elem: '#test'
       , url: "https://api.mlwei.com/music/api/wy/?key=523077333&type=so&nu=30&id=" + wyid
       , title: 'item'
-      ,toolbar: "turn"
+      , toolbar: "turn"
       , cols: [[
         { field: 'id', title: 'ID', type: "normal", }
         , { field: 'title', title: '曲名', type: "normal" }
         , { field: 'author', title: '歌手', type: "normal" }
         , { field: 'url', title: '地址', type: "normal" }
-        ,{fixed: 'right', title:'操作', toolbar: '#barDemo', width:150}
+        , { fixed: 'right', title: '操作', toolbar: '#barDemo', width: 150 }
       ]]
       , response: {
         statusName: "Code",
@@ -58,3 +59,19 @@ function BcopyText() {
     });
   });
 };
+layui.use('table', function () {
+  var table = layui.table;
+  table.on('tool(test)', function (obj) {
+    var id = obj.id;
+    console.log(obj)
+    if (obj.event === 'turn in') {
+      
+
+
+
+
+
+
+    };
+  });
+});
